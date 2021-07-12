6344733467b2412aa28728c35001066da736e604
Philippe Bernard <philippe.bernard@gmail.com>
2021-07-12 14:39:22 +0200

    Message added to describe the issue and make it reproductible

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
+  // When target is not set, the build succeeds
+  // When target is set to serverless, the build fails
+  //target: 'serverless',
+
   reactStrictMode: true,
   i18n
 }
